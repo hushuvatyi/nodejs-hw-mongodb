54deb5a1a09a221627f5a72bebee2b2e2e023101
unknown <andrii.hushuvatyi@gmail.com>
2025-01-16 10:07:49 +0200

    hw2 add services

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -1,11 +1,11 @@
 import { ContactsCollection } from '../db/models/contact.js';
 
 export const getAllContacts = async () => {
-  const students = await ContactsCollection.find();
-  return students;
+  const contacts = await ContactsCollection.find();
+  return contacts;
 };
 
 export const getContactById = async (contactId) => {
-  const student = await ContactsCollection.findById(contactId);
-  return student;
+  const contact = await ContactsCollection.findById(contactId);
+  return contact;
 };
